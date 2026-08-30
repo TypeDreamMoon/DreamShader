00044748834a650f5b6ab0d62cdeebe6ca2dbc8f
TypeDreamMoon <1067823908@qq.com>
2026-08-30 17:50:21 +0800

    The Dream combo is a simple combo: the whole button opens the menu (field-tested split-button dead zone)

--- Source/DreamShaderEditor/Private/Bridge/DreamShaderEditorBridge.cpp
+++ Source/DreamShaderEditor/Private/Bridge/DreamShaderEditorBridge.cpp
@@ -1422,7 +1422,8 @@ namespace UE::DreamShader::Editor::Private
 				LOCTEXT("DreamToolsComboLabel", "Dream"),
 				LOCTEXT("DreamToolsComboTooltip",
 					"Open a Dream-family source workspace (DreamShader / DreamFX / DreamUI) in VSCode; Notepad stands in when VSCode is unavailable."),
-				FSlateIcon(FAppStyle::GetAppStyleSetName(), TEXT("Icons.OpenInExternalEditor"))));
+				FSlateIcon(FAppStyle::GetAppStyleSetName(), TEXT("Icons.OpenInExternalEditor")),
+				/*bInSimpleComboBox*/ true));
 		}
 	}
 
